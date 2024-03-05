88e04c98dddd2dd7e2be2e2b4e38f73c3a5cb7ff
Lukman Hakim <madlukmanhakim@gmail.com>
2024-03-05 22:00:32 +0700

    W03: Jawaban soal 7

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## Soal 1
 ![gambar 1](1.png)
-
+Menambahkan domain website imgur pada next.config.mjs
 ## Soal 2
 ![alt text](2.png)
-
+Membuat komponen baru bernama Gallery yang akan memanggil Profile sebanyak 3x
 ## Soal 3
 ![alt text](3.png)
 
@@ -12,9 +12,12 @@ Kode Sudah Dicommit
 
 ## Soal 5
 ![alt text](4.png)
-
+Tidak ada perbedaan
 ## Soal 6
+![alt text](image.png)
 
+Tidak ada perbedaan
 ## Soal 7
+![alt text](7.png)
 ## Soal 8
 ## Soal 9
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Gallery } from "@/components/gallery";
 import Image from "next/image";
 import TodoList from "@/components/todolist";
+import MyGallery from "@/components/mygallery";
 
 
 // export default function Home() {
@@ -33,10 +34,7 @@ export default function Bio() {
   return (
      <section>
       <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuwan yang luar biasa</h1>
-      <hr />
-      <Gallery />
-      <hr />
-      <TodoList />
+      <MyGallery />
     </section>
    
   )
