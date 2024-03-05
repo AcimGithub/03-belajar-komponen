275b1426e848f6092df2c512b29ef7bb313485e2
Lukman Hakim <madlukmanhakim@gmail.com>
2024-03-05 20:07:41 +0700

    W03: Jawaban soal 4

--- next.config.mjs
+++ next.config.mjs
@@ -4,5 +4,5 @@ export default {
     images: {
       domains: ['i.imgur.com'],
     },
-  };
+  }
   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
 import { Gallery } from "@/components/gallery";
 import Image from "next/image";
 
-export default function Home() {
+
+// export default function Home() {
+//   return (
+//     <div className="center">
+//       <p style={{ textAlign: 'center' }}>
+//         Ilmuwan yang luar biasa
+//       </p>
+//       <Gallery/>
+//     </div>
+//   );
+// }
+
+export default function Bio() {
   return (
-    <div className="center">
-      <p style={{ textAlign: 'center' }}>
-        Ilmuwan yang luar biasa
-      </p>
-      <Gallery/>
+    <>
+     <div className="intro">
+      <h1>Selamat datang di website saya!</h1>
     </div>
-  );
+    <p>
+      Anda dapat membaca uneg-uneg.
+      <br />
+      <b>Juga ada <i>foto</i> ilmuwan!</b>
+    </p>
+    
+    </>
+   
+  )
 }
 
